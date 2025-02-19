327436187763e5a8707de300990fd727f59610d9
Hugh Geng <gengjian1203@foxmail.com>
2025-02-19 11:56:00 +0800

    feat(editorInjectTestId): add editorInjectTestId

--- src/command/editorInjectTestId/index.ts
+++ src/command/editorInjectTestId/index.ts
@@ -4,102 +4,91 @@ import { Element } from "domhandler";
 import * as fs from "fs";
 // import serializer from "dom-serializer";
 
-const injectTagsList = ["div"];
-const selfClosingTagsList = [
-  "img",
-  "br",
-  "hr",
-  "input",
-  "meta",
-  "link",
-  "area",
-  "base",
-  "col",
-  "command",
-  "embed",
-  "keygen",
-  "param",
-  "source",
-  "track",
-  "wbr",
-];
-const explicitAttrsList = ["alt", "title", "nzErrorTip", "nzValue"];
-
-const addDataTestId = (element: Element) => {
-  if (
-    !element.attribs["data-testid"] &&
-    injectTagsList.includes(element.tagName)
-  ) {
-    element.attribs["data-testid"] = `${element.tagName}-[test-id-name]`;
-  }
-
-  if (element.children) {
-    element.children.forEach((child: any) => {
-      if (child instanceof Element) {
-        addDataTestId(child);
-      }
-    });
-  }
-};
-
-const render = (dom: any) => {
-  // 递归处理每个节点
-  const renderNode = (node: any): string => {
-    // 如果是文本节点，直接返回文本内容
-    if (node.type === "text") {
-      return node.data;
-    }
-
-    // 如果是元素节点
-    if (node.type === "tag") {
-      // 处理属性
-      let attrs = Object.entries(node.attribs || {})
-        .map(([key, value]) => {
-          // TODO: 如果为空字符，未必是只有key没有value，临时如此处理
-          if (!explicitAttrsList.includes(key) && value === "") {
-            return `${key}`;
-          }
-          // 保持原有的属性值,包括空字符串
-          return `${key}="${value}"`;
-        })
-        .join(" ");
-
-      attrs = !!attrs ? ` ${attrs}` : ``;
-
-      // 如果是自闭合标签
-      if (node.selfClosing || selfClosingTagsList.includes(node.name)) {
-        return `<${node.name}${attrs}/>`;
-      }
-
-      // 处理子节点
-      const children = node.children
-        .map((child: any) => renderNode(child))
-        .join("");
-
-      return `<${node.name}${attrs}>${children}</${node.name}>`;
-    }
-
-    return "";
-  };
-
-  // 渲染整个文档
-  return dom.children.map((child: any) => renderNode(child)).join("");
-};
-
 export default (context: vscode.ExtensionContext) => {
   return vscode.commands.registerCommand(
     "code-maker.editor.InjectTestId",
     (res) => {
       // The code you place here will be executed every time your command is executed
-      vscode.window.showInformationMessage("editorInjectTestId");
-      console.log("editorInjectTestId", res);
-
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
         vscode.window.showErrorMessage("No active editor found.");
         return;
       }
 
+      const vscodeConfig = vscode.workspace.getConfiguration();
+      const injectTags = vscodeConfig.get("code-maker.injectTestId.injectTags");
+      const selfClosingTags = vscodeConfig.get(
+        "code-maker.injectTestId.selfClosingTags"
+      );
+      const explicitAttrs = vscodeConfig.get(
+        "code-maker.injectTestId.explicitAttrs"
+      );
+      const injectTagsList = injectTags?.toString().split("|") || [];
+      const selfClosingTagsList = selfClosingTags?.toString().split("|") || [];
+      const explicitAttrsList = explicitAttrs?.toString().split("|") || [];
+
+      const addDataTestId = (element: Element) => {
+        if (
+          !element.attribs["data-testid"] &&
+          injectTagsList.includes(element.tagName)
+        ) {
+          element.attribs["data-testid"] = `${element.tagName}-[test-id-name]`;
+        }
+
+        if (element.children) {
+          element.children.forEach((child: any) => {
+            if (child instanceof Element) {
+              addDataTestId(child);
+            }
+          });
+        }
+      };
+
+      const render = (dom: any) => {
+        // 递归处理每个节点
+        const renderNode = (node: any): string => {
+          // 如果是文本节点，直接返回文本内容
+          if (node.type === "text") {
+            return node.data;
+          }
+
+          // 如果是元素节点
+          if (node.type === "tag") {
+            // 处理属性
+            let attrs = Object.entries(node.attribs || {})
+              .map(([key, value]) => {
+                // TODO: 如果为空字符，未必是只有key没有value，临时如此处理
+                if (!explicitAttrsList.includes(key) && value === "") {
+                  return `${key}`;
+                }
+                // 保持原有的属性值,包括空字符串
+                return `${key}="${value}"`;
+              })
+              .join(" ");
+
+            attrs = !!attrs ? ` ${attrs}` : ``;
+
+            // 如果是自闭合标签
+            if (node.selfClosing || selfClosingTagsList.includes(node.name)) {
+              return `<${node.name}${attrs}/>`;
+            }
+
+            // 处理子节点
+            const children = node.children
+              .map((child: any) => renderNode(child))
+              .join("");
+
+            return `<${node.name}${attrs}>${children}</${node.name}>`;
+          }
+
+          return "";
+        };
+
+        // 渲染整个文档
+        return dom.children.map((child: any) => renderNode(child)).join("");
+      };
+
+      // start...
       const document = editor.document;
       if (document.languageId !== "html") {
         vscode.window.showErrorMessage("The active file is not an HTML file.");
